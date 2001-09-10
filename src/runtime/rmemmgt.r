8c4cb3349c0b96908f9dc054f5e7052632d11ab0
Clinton Jeffery <clint.jeffery@gmail.com>
2001-09-10 23:32:07 +0000

    cast fixes

--- src/runtime/rmemmgt.r
+++ src/runtime/rmemmgt.r
@@ -478,7 +478,7 @@ int region;
    { struct region *br;
    for (br = curblock->Gnext; br; br = br->Gnext) {
       char *source = br->base, *free = br->free;
-      uword NoMark = ~F_Mark;
+      uword NoMark = (uword) ~F_Mark;
       while (source < free) {
 	 BlkType(source) &= NoMark;
          source += BlkSize(source);
@@ -486,7 +486,7 @@ int region;
       }
    for (br = curblock->Gprev; br; br = br->Gprev) {
       char *source = br->base, *free = br->free;
-      uword NoMark = ~F_Mark;
+      uword NoMark = (uword) ~F_Mark;
       while (source < free) {
 	 BlkType(source) &= NoMark;
          source += BlkSize(source);
@@ -1204,7 +1204,7 @@ word extra;
     *  locations.
     */
    qsort((char *)quallist, (int)(DiffPtrs((char *)qualfree,(char *)quallist)) /
-     sizeof(dptr *), sizeof(dptr), (int (*)(const void *, const void *))qlcmp);
+     sizeof(dptr *), sizeof(dptr), (QSortFncCast)qlcmp);
    /*
     * The string qualifiers are now ordered by starting location.
     */

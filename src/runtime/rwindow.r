c5afac385114362959d932f7f2dd591389da184d
Clinton Jeffery <clint.jeffery@gmail.com>
2001-09-11 08:05:58 +0000

    cfunc graphics fix

--- src/runtime/rwindow.r
+++ src/runtime/rwindow.r
@@ -3414,7 +3414,13 @@ stringint attribs[] = {
  */
 
 #else					/* Graphics */
-static char junk;		/* avoid empty module */
+
+/*
+ * Stubs to prevent dynamic loader from rejecting cfunc library of IPL.
+ */
+int palnum(dptr *d)					{ return 0; }
+char *rgbkey(int p, double r, double g, double b)	{ return 0; }
+
 #endif					/* Graphics */
 
 #if defined(Graphics) || defined(PosixFns)

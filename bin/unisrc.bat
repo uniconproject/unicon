5b09b25b0ff1710c4b900a4d7fe3acbfa009cbd1
Clinton Jeffery <clint.jeffery@gmail.com>
2002-02-26 09:27:57 +0000

    remove CVS subdirectories from source distribution build scripts

--- bin/unisrc.bat
+++ bin/unisrc.bat
@@ -1,7 +1,7 @@
 cd \unicon\v
 del uni.zip
 zip -r uni.zip Makefile README
-zip -r uni.zip src\Makefile src\h
+zip -r uni.zip src\Makefile src\h\*.h
 zip -r uni.zip src\icont\*.c src\icont\*.h src\icont\*.icn src\icont\Makefile
 zip -r uni.zip src\common\*.c src\common\*.h src\common\*.icn src\common\Makefile
 zip -r uni.zip src\iconc\*.c src\iconc\*.h src\iconc\*.icn src\iconc\Makefile
@@ -11,15 +11,50 @@ zip -r uni.zip src\rtt\*.c src\rtt\*.h src\rtt\*.icn src\rtt\Makefile
 zip -r uni.zip src\runtime\*.r src\runtime\*.ri src\runtime\*.icn src\runtime\Makefile
 zip -r uni.zip src\gdbm\*.c src\gdbm\*.h src\gdbm\gdbm.mak src\gdbm\configure.in src\gdbm\autoconf.h.in src\gdbm\autoconf.$$$ src\gdbm\gdbm.lib
 zip -r uni.zip src\wincap\*.c src\wincap\*.h src\wincap\Makefile
-zip -r uni.zip uni\ivib\*.icn uni\ivib\makefile uni\ivib\icon uni\ivib\docs uni\ivib\demos
+zip -r uni.zip uni\ivib\*.icn uni\ivib\makefile uni\ivib\icon\i*.*
+zip -r uni.zip uni\ivib\icon\x*.* uni\ivib\docs uni\ivib\demos
 zip -r uni.zip uni\Makefile uni\docs uni\ide uni\lib\*.icn uni\lib\makefile
 zip -r uni.zip uni\iyacc\*.c uni\iyacc\*.h uni\iyacc\Makefile
 zip -r uni.zip uni\iyacc\test\Makefile uni\iyacc\test\calc.* uni\iyacc\test\make.bat uni\iyacc\test\makefile.win
 zip -r uni.zip uni\unicon\*.icn uni\unicon\Makefile uni\unicon\*.y
 uni\unicon\unigram.u uni\unicon\idol.u
 zip -r uni.zip config\nt\msvc
-zip -r uni.zip ipl\README ipl\Translate-icont ipl\cfuncs ipl\data ipl\docs ipl\gdata ipl\gdocs ipl\gpacks ipl\mdata ipl\mdocs ipl\mpacks ipl\packs ipl\lib\makefile
+zip -r uni.zip ipl\README ipl\Translate-icont
+zip -r uni.zip ipl\cfuncs\*.c ipl\cfuncs\*.h ipl\cfuncs\*.sh ipl\cfuncs\Makefile ipl\cfuncs\README
+zip -r uni.zip ipl\data\README ipl\data\*.??? ipl\data\header
+zip -r uni.zip ipl\docs\README ipl\docs\*.??? ipl\docs\*.1
+zip -r uni.zip ipl\gdata\README ipl\gdata\*.??? ipl\gdata\*.ed
+zip -r uni.zip ipl\gdocs\README ipl\gdocs\*.?dx ipl\gdocs\*.ps
+zip -r uni.zip ipl\gpacks\README
+zip -r uni.zip ipl\gpacks\carpets\*.icn ipl\gpacks\carpets\Makefile ipl\gpacks\carpets\README
+zip -r uni.zip ipl\gpacks\drawtree\*.icn ipl\gpacks\drawtree\Makefile ipl\gpacks\drawtree\README
+zip -r uni.zip ipl\gpacks\ged\*.icn ipl\gpacks\ged\Makefile ipl\gpacks\ged\README
+zip -r uni.zip ipl\gpacks\htetris\*.icn ipl\gpacks\htetris\Makefile ipl\gpacks\htetris\README
+zip -r uni.zip ipl\gpacks\tiger\*.icn ipl\gpacks\tiger\Makefile ipl\gpacks\tiger\README
+zip -r uni.zip ipl\gpacks\vib\*.icn ipl\gpacks\vib\Makefile ipl\gpacks\vib\README
+zip -r uni.zip ipl\gpacks\weaving\*.icn ipl\gpacks\weaving\Makefile ipl\gpacks\weaving\README
+zip -r uni.zip ipl\gpacks\xtiles\*.icn ipl\gpacks\xtiles\Makefile ipl\gpacks\xtiles\README
+zip -r uni.zip ipl\mdata\README ipl\mdocs\README ipl\mdocs\m*.* ipl\mpacks\README
+zip -r uni.zip ipl\packs\README ipl\lib\makefile
+zip -r uni.zip ipl\packs\euler\README ipl\packs\euler\Makefile ipl\packs\euler\*.???
+zip -r uni.zip ipl\packs\ibpag2\README ipl\packs\ibpag2\Makefile ipl\packs\ibpag2\*.???
+zip -r uni.zip ipl\packs\idol\README ipl\packs\idol\Makefile ipl\packs\idol\*.???
+zip -r uni.zip ipl\packs\itweak\README ipl\packs\itweak\Makefile ipl\packs\itweak\*.???
+zip -r uni.zip ipl\packs\loadfunc\README ipl\packs\loadfunc\Makefile ipl\packs\loadfunc\*.???
+zip -r uni.zip ipl\packs\skeem\README ipl\packs\skeem\Makefile ipl\packs\skeem\*.???
+zip -r uni.zip ipl\packs\tcll1\README ipl\packs\tcll1\Makefile ipl\packs\tcll1\*.???
 zip -r uni.zip ipl\gincl\*.icn ipl\gprocs\*.icn ipl\gprogs\*.icn
 zip -r uni.zip ipl\incl\*.icn ipl\procs\*.icn ipl\progs\*.icn
 zip -r uni.zip ipl\mincl\*.icn ipl\mprocs\*.icn ipl\mprogs\*.icn
-zip -r uni.zip tests
+zip -r uni.zip tests\M* tests\R*
+zip -r uni.zip tests\bench\Comp* tests\bench\M* tests\bench\R* tests\bench\T* tests\bench\*.???
+zip -r uni.zip tests\calling\M* tests\calling\R* tests\calling\extcall\M* tests\calling\extcall\e*.* tests\calling\loadfunc\M* tests\calling\loadfunc\R* tests\calling\loadfunc\b*.* tests\calling\loadfunc\mklib.sh
+zip -r uni.zip tests\general\Makefile tests\general\README tests\general\T* tests\general\*.???
+zip -r uni.zip tests\graphics\Makefile tests\graphics\README tests\graphics\gpxtest.*
+zip -r uni.zip tests\ipl\Makefile tests\ipl\Test tests\ipl\*.icn tests\ipl\stand\*.out
+zip -r uni.zip tests\posix\Makefile tests\posix\T* tests\posix\*.icn tests\posix\*.lst tests\posix\stand\*.out tests\posix\test-dir\?
+zip -r uni.zip tests\preproc\Makefile tests\preproc\README tests\preproc\T* tests\preproc\*.? tests\preproc\stand\*.out tests\preproc\dir2\*.h tests\preproc\dir1\*.h tests\preproc\dir1\dir11\*.h
+zip -r uni.zip tests\samples\Makefile tests\samples\T* tests\samples\*.??? tests\samples\stand\*.out
+zip -r uni.zip tests\special\Makefile tests\special\README tests\special\*.???
+zip -r uni.zip tests\unicon\*.icn
+

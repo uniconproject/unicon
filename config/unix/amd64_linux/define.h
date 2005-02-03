19d4aa1f423ccb12ba8e21c849fc599d60f81676
Clinton Jeffery <clint.jeffery@gmail.com>
2005-02-03 04:13:19 +0000

    amd64_linux configuration

---
+++ config/unix/amd64_linux/define.h
@@ -0,0 +1,26 @@
+/*  
+ * Icon configuration file for Linux v2 on Athlon64
+ */
+
+#define UNIX 1
+#define LoadFunc
+/* no SysOpt: Linux getopt() is POSIX-compatible only if an envmt var is set */
+
+#define CComp "gcc"
+#define COpts "-O2 -fomit-frame-pointer"
+#define NEED_UTIME
+
+/* CPU architecture */
+#define IntBits 32
+#define WordBits 64
+#define Double
+#define StackAlign 16
+#define CComp "gcc"
+#define COpts "-O2 -fomit-frame-pointer"
+#define NEED_UTIME
+
+/*
+ * Some Linuxen have -lcrypt, some dont.
+ * If you have crypt, you can remove this.
+ */
+#define NoCrypt

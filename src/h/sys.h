3abc31aab96f16eaf24e40de5c7534e473da9473
Clinton Jeffery <clint.jeffery@gmail.com>
2005-02-08 04:09:47 +0000

    libjpeg compile issues on amd64

--- src/h/sys.h
+++ src/h/sys.h
@@ -18,6 +18,24 @@
 #include <string.h>
 #include <time.h>
 
+#if HAVE_LIBJPEG
+
+#if defined(__x86_64__) && defined(XWindows)
+/* Some AMD64 Gentoo systems seem to have a buggy macros in
+   jmorecfg.h, but if we include Xmd.h beforehand then we get better
+   definitions of the macros. */
+#include <X11/Xmd.h>
+#endif
+
+#include "jpeglib.h"
+#include "jerror.h"
+#include <setjmp.h>
+/* we do not use their definitions of GLOBAL, LOCAL, or OF; we use our own */
+#undef GLOBAL
+#undef LOCAL
+#undef OF
+#endif					/* HAVE_LIBJPEG */
+
 /*
  * Operating-system-dependent includes.
  */

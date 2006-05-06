a108f5b343752f1491592d3c3a9111ded3722bb3
Clinton Jeffery <clint.jeffery@gmail.com>
2006-05-06 10:26:17 +0000

    introduce audio facilities header

---
+++ src/h/audio.h
@@ -0,0 +1,11 @@
+/*
+ * audio.h - audio structure used in Unicon's audio interface
+ */
+
+ struct AudioFile{
+	int doneflag;
+	char* fname;
+	};
+
+typedef struct AudioFile AudioStruct;
+typedef struct AudioFile * AudioPtr;

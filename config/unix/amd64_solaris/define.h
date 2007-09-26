3b1b09373dcd164571581174840099bc10306df1
Clinton Jeffery <clint.jeffery@gmail.com>
2007-09-26 05:35:17 +0000

    preliminary amd64 solaris support

---
+++ config/unix/amd64_solaris/define.h
@@ -0,0 +1,21 @@
+/*  
+ * Icon configuration file for an AMD64 system running Solaris 10,
+   Sun Studio 12 compilers.
+*/
+
+#define UNIX 1
+#define INTMAIN /* int main() */
+
+#define LoadFunc 1
+#define SysOpt   1
+#define NoRanlib
+
+/* CPU architecture */
+#define IntBits 32
+#define WordBits 64
+#define Double
+#define StackAlign 16
+
+#define NEED_UTIME
+#define Messaging  1
+#define PosixFns   1

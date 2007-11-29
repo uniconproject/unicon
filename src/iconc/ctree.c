0a7f32522e75d1f47b78be7dd13d2920facd4642
qzx <qzx@users.sf.net>
2007-11-29 02:24:04 +0000

    modified invk_check_dyn_rec to use struct treenode n_nfld member

--- src/iconc/ctree.c
+++ src/iconc/ctree.c
@@ -326,7 +326,7 @@ invk_check_dyn_rec(argc, argv)
    if (argc < 2)
       return -1;
    p = argv->n_field[1].n_ptr;
-   if (p->n_field[0].csym < 256 || p->n_field[0].csym->image == 0)
+   if (p->n_nflds <= 0 || p->n_field[0].csym->image == 0)
       return -1;
    if (strcmp("constructor", p->n_field[0].csym->image) != 0)
       return -1;

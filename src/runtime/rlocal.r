0035f6acd59217ce373205d085f303a6de1e7dc1
Clinton Jeffery <clint.jeffery@gmail.com>
2008-01-16 10:05:48 +0000

    test popen improvement

--- src/runtime/rlocal.r
+++ src/runtime/rlocal.r
@@ -1027,6 +1027,75 @@ FILE *ptr;
 
 #if UNIX
 
+#if YouArePtho
+/*
+ * Modified     3/03/2001       Manuel Novoa III
+ *
+ * Added check for legal mode arg.
+ * Call fdopen and check return value before forking.
+ * Reduced code size by using variables pr and pnr instead of array refs.
+ */
+
+#if ! defined __NR_vfork && defined __UCLIBC_HAS_MMU__ 
+#define vfork fork	
+#endif
+
+FILE *popen (const char *command, const char *mode)
+{
+   FILE *fp;
+   int pipe_fd[2];
+   int pid, reading;
+   int pr, pnr;
+
+   reading = (mode[0] == 'r');
+   if ((!reading && (mode[0] != 'w')) || mode[1]) {
+#if 0
+	__set_errno(EINVAL);			/* Invalid mode arg. */
+#else
+	errno = EINVAL;
+#endif
+      }
+   else if (pipe(pipe_fd) == 0) {
+	pr = pipe_fd[reading];
+	pnr = pipe_fd[1-reading];
+	if ((fp = fdopen(pnr, mode)) != NULL) {
+		if ((pid = vfork()) == 0) {	/* vfork -- child */
+			close(pnr);
+			close(reading);
+			if (pr != reading) {
+				dup2(pr, reading);
+				close(pr);
+			}
+			execl("/bin/sh", "sh", "-c", command, (char *) 0);
+			_exit(255);		/* execl failed! */
+		} else {			/* vfork -- parent or failed */
+			close(pr);
+			if (pid > 0) {	/* vfork -- parent */
+				return fp;
+			} else {		/* vfork -- failed! */
+				fclose(fp);
+			}
+		}
+	} else {				/* fdopen failed */
+		close(pr);
+		close(pnr);
+	}
+   }
+   return NULL;
+}
+
+int pclose(FILE *fd)
+{
+	int waitstat;
+
+	if (fclose(fd) != 0) {
+		return EOF;
+	}
+	wait(&waitstat);
+	return waitstat;
+}
+#endif
+
 /*
  * Documentation notwithstanding, the Unix versions of the keyboard functions
  * read from standard input and not necessarily from the keyboard (/dev/tty).

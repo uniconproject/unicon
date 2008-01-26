7ef3bfaf385d7f396863f3754a55c79de43e0758
Clinton Jeffery <clint.jeffery@gmail.com>
2008-01-26 08:34:23 +0000

    OS X needs 16-byte alignment

--- config/unix/intel_macos/define.h
+++ config/unix/intel_macos/define.h
@@ -2,7 +2,9 @@
  * Icon configuration file for PPC running Darwin 1.4 or newer.
  */
 
+#define StackAlign 16
 #define UNIX 1
 #define NEED_UTIME
+#define NamedSemaphores
 /* LoadFunc not implemented */
 

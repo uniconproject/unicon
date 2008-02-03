4dc28e0801e74ba7c0d904d7e1b3226eb230747d
qzx <qzx@users.sf.net>
2008-02-03 06:47:21 +0000

    added ca_cleanup function

--- src/iconc/ca.h
+++ src/iconc/ca.h
@@ -2,6 +2,7 @@
 #define CA_H __DATE__" "__TIME__
 
 extern int ca_apply_add(char *, struct node *);
+extern int ca_cleanup(void);
 extern int ca_init(char *, int, char **);
 extern int ca_invk_add(char *, struct node *);
 extern int ca_mark_parsed(char *);

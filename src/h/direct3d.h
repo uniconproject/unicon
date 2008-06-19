f0b6950e944da80975067da21d0ee6a102aa7803
Clinton Jeffery <clint.jeffery@gmail.com>
2008-06-19 02:40:28 +0000

    Direct3d placeholder; for future work

---
+++ src/h/direct3d.h
@@ -0,0 +1,10 @@
+/*
+ * Direct 3D port of the 3D facilities. Not implemented yet.
+ */
+
+#define U3D_LINES      1
+#define U3D_LINE_STRIP 2
+#define U3D_POINTS     3
+#define U3D_REPLACE    4
+#define U3D_BLEND      5
+#define U3D_MODULATE   6

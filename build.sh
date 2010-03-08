91cd41de9fbae3f58a9560b6bbd5112f63d7d482
Clinton Jeffery <clint.jeffery@gmail.com>
2010-03-08 21:47:50 +0000

    generalize to recognize more configurations

--- build.sh
+++ build.sh
@@ -5,12 +5,30 @@
 # for UNIX-based configurations.
 #
 
-SYS=`uname -ms | sed 's/ /_/' | sed 's/x86_64/amd64/' | sed 's/Linux/linux/' | sed 's/x86/intel/'`
+SYS=`uname -a | sed 's/ /_/g'`
 case "$SYS" in
-   linux_amd64)
-      make build name=amd64_linux;;
-   linux_intel)
+   Darwin*Version_10*i386)
+      make build name=amd64_macos;;
+   Darwin*Version_9*i386)
+      make build name=intel_macos;;
+   Darwin*Power_Macintosh)
+      make build name=ppc_macos;;
+   Darwin*powerpc)
+      make build name=ppc_macos;;
+   FreeBSD*i386)
+      make build name=intel_freebsd;;
+   Linux*alpha*)
+      make build name=alpha_linux;;
+   Linux*i686*)
       make build name=intel_linux;;
+   Linux*sparc64*)
+      make build name=sun_linux;;
+   Linux*x86_64*)
+      make build name=amd64_linux;;
+   SunOS*sparc*)
+      make build name=sparc_solaris;;
+   SunOS*i86pc)
+      make build name=amd64_solaris;;
    *)
       echo 1>&2 "don't know how to select configuration for $SYS, use 'make [X-]Configure name=system'"
       exit 1;;

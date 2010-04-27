d1758933f563048620635849525913f80fafd26e
Clinton Jeffery <clint.jeffery@gmail.com>
2010-04-27 21:58:04 +0000

    pthreads preparation

--- src/h/grttin.h
+++ src/h/grttin.h
@@ -360,6 +360,10 @@ typedef int siptr, stringint, inst;
       typedef int PBITMAPINFOHEADER, MinBitmapHeader, RGB;
    #endif				/* PresentationManager */
 
+#ifdef HAVE_LIBPTHREAD
+       typedef int pthread_rwlock_t;
+#endif					/* HAVE_LIBPTHREAD */
+
    /*
     * Convenience macros to make up for RTL's long-windedness.
     */

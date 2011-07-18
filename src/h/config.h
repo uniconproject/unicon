d5d655accc4c9fffaa697d18e1906ade86b26fda
Clinton Jeffery <clint.jeffery@gmail.com>
2011-07-18 06:40:27 +0000

    comment fixes

--- src/h/config.h
+++ src/h/config.h
@@ -322,10 +322,10 @@
       #define ICONC_XLIB "-L/usr/X11R6/lib -lX11"
    #endif				/* ICONC_XLIB */
 
-#if defined(ConsoleWindow) || (NT && !defined(Rttx))  /*1 def ConsoleWindow*/
+#if defined(ConsoleWindow) || (NT && !defined(Rttx))
       /*
-       * knock out fprintf and putc; these are here so that consoles may be used
-       * in icont and rtt, not just iconx
+       * Knock out fprintf and putc; these are here so that consoles
+       * may be used in icont and rtt, not just iconx.
        */
       #undef fprintf
       #define fprintf Consolefprintf

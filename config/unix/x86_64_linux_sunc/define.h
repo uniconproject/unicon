29f6984e81c826dbe6c61fca6ebb19215cd0200a
Clinton Jeffery <clint.jeffery@gmail.com>
2012-01-02 02:16:27 +0000

    removed unnecessary lines per Kostas Oikonomou

--- config/unix/x86_64_linux_sunc/define.h
+++ config/unix/x86_64_linux_sunc/define.h
@@ -1,22 +1,16 @@
-/*  
- * Icon configuration file for Linux v2 on Athlon64
+/*
+ * Icon configuration file for Linux v2 on Athlon64, Solaris Studio compilers
  */
 
 #define UNIX 1
 #define LoadFunc
 /* no SysOpt: Linux getopt() is POSIX-compatible only if an envmt var is set */
 
-#define CComp "gcc"
-#define COpts "-O2 -fomit-frame-pointer"
-#define NEED_UTIME
-
 /* CPU architecture */
 #define IntBits 32
 #define WordBits 64
 #define Double
 #define StackAlign 16
-#define CComp "gcc"
-#define COpts "-O2 -fomit-frame-pointer"
 #define NEED_UTIME
 /* a non-default datatype for SQLBindCol's 6th parameter */
 #define SQL_LENORIND SQLLEN
@@ -26,3 +20,5 @@
  * If you have crypt, you can remove this.
  */
 #define NoCrypt
+#define Concurrent 1
+#define PTHREAD_MUTEX_RECURSIVE PTHREAD_MUTEX_RECURSIVE_NP

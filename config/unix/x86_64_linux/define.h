058943f7096f53fd81eed13042f0f58bda94b500
Clinton Jeffery <clint.jeffery@gmail.com>
2012-09-01 18:22:32 +0000

    instructions on PatternType

--- config/unix/x86_64_linux/define.h
+++ config/unix/x86_64_linux/define.h
@@ -33,4 +33,6 @@
  * you might need to
  * #define PTHREAD_MUTEX_RECURSIVE PTHREAD_MUTEX_RECURSIVE_NP
  * as well.
+ * #define PatternType 1
+ * here to enable (experimental) SNOBOL-style pattern type.
  */

c00a3145e248fae305f2561b4595a63a8fd2866d
Clinton Jeffery <clint.jeffery@gmail.com>
2013-03-05 20:01:04 +0000

    reorg/expand portable 3D defines

--- src/h/direct3d.h
+++ src/h/direct3d.h
@@ -2,12 +2,30 @@
  * Direct 3D port of the 3D facilities. Not implemented yet.
  */
 
-#define U3D_LINES      1
-#define U3D_LINE_STRIP 2
-#define U3D_POINTS     3
-#define U3D_REPLACE    4
-#define U3D_BLEND      5
-#define U3D_MODULATE   6
+/*
+ * Redefinitions of various OpenGL integer scalar values.
+ * Using these names, portable code can function without OpenGL.
+ * If OpenGL is present, they are defined as OpenGL-native, see opengl.h
+ * On other platforms, they may be a simple integer enumeration or may be
+ * redefined to values native to that platform.
+ */
+
+#define U3D_POINTS         1
+#define U3D_LINES          2
+#define U3D_LINE_STRIP     3
+#define U3D_LINE_LOOP      4
+#define U3D_TRIANGLES      5
+#define U3D_TRIANGLE_FAN   6
+#define U3D_TRIANGLE_STRIP 7
+#define U3D_QUADS          8
+#define U3D_QUAD_STRIP     9
+#define U3D_POLYGON       10
+
+#define U3D_REPLACE      100
+#define U3D_BLEND        101
+#define U3D_MODULATE     102
 
 #define GLfloat float
 #define GLubyte unsigned char
+#define GLuint unsigned int
+
--- src/h/opengl.h
+++ src/h/opengl.h
@@ -1,6 +1,20 @@
-#define U3D_LINES GL_LINES
-#define U3D_LINE_STRIP GL_LINE_STRIP
-#define U3D_POINTS GL_POINTS
-#define U3D_REPLACE GL_REPLACE
-#define U3D_BLEND GL_BLEND
-#define U3D_MODULATE GL_MODULATE
+/*
+ * mesh modes.
+ */
+#define U3D_POINTS         GL_POINTS
+#define U3D_LINES          GL_LINES
+#define U3D_LINE_STRIP     GL_LINE_STRIP
+#define U3D_LINE_LOOP      GL_LINE_LOOP
+#define U3D_TRIANGLES      GL_TRIANGLES
+#define U3D_TRIANGLE_FAN   GL_TRIANGLE_FAN
+#define U3D_TRIANGLE_STRIP GL_TRIANGLE_STRIP
+#define U3D_QUADS          GL_QUADS
+#define U3D_QUAD_STRIP     GL_QUAD_STRIP
+#define U3D_POLYGON        GL_POLYGON
+
+/*
+ * texture modes.
+ */
+#define U3D_REPLACE        GL_REPLACE
+#define U3D_BLEND          GL_BLEND
+#define U3D_MODULATE       GL_MODULATE

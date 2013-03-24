2ae2c7c917706cdaaaa3a5a023f927d66c39d69a
Clinton Jeffery <clint.jeffery@gmail.com>
2013-03-24 04:21:55 +0000

    minor cleanup

--- src/common/drawstring3d.cc
+++ src/common/drawstring3d.cc
@@ -8,11 +8,7 @@
 
 #if HAVE_LIBGL && HAVE_LIBFTGL
 #include <GL/gl.h>
-// #include <GL/glut.h>
-
-
 #include <FTGL/ftgl.h>
-
 
 static FTFont *fonts;
 // static int width;
@@ -44,9 +40,6 @@ extern "C" int cpp_drawstring3d(double x, double y, double z, char *s,
       printf("ERROR: Unable to set font face size %d\n", point_size);
       return 1;
       }
-#if 0
-   glColor3f(1.0, 1.0, 1.0);
-#endif
 
    if (t == 2) {
       glEnable(GL_TEXTURE_2D);
@@ -76,72 +69,3 @@ extern "C" int cpp_drawstring3d(double x, double y, double z, char *s,
 }
 
 #endif					/* HAVE_LIBGL && HAVE_FTGL */
-
-/*
-static void
-do_ortho()
-{
-  int w;
-  int h;
-  GLdouble size;
-  GLdouble aspect;
-
-  w = width;
-  h = height;
-  aspect = (GLdouble)w / (GLdouble)h;
-
-  // Use the whole window.
-  glViewport(0, 0, w, h);
-
-  // We are going to do some 2-D orthographic drawing.
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  size = (GLdouble)((w >= h) ? w : h) / 2.0;
-  if (w <= h) {
-    aspect = (GLdouble)h/(GLdouble)w;
-    glOrtho(-size, size, -size*aspect, size*aspect,
-            -100000.0, 100000.0);
-  }
-  else {
-    aspect = (GLdouble)w/(GLdouble)h;
-    glOrtho(-size*aspect, size*aspect, -size, size,
-            -100000.0, 100000.0);
-  }
-
-  // Make the world and window coordinates coincide so that 1.0 in
-  // model space equals one pixel in window space.
-  glScaled(aspect, aspect, 1.0);
-
-   // Now determine where to draw things.
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-}
-*/
-
-/*
-void
-draw_scene()
-{
-   glColor3f(1.0, 1.0, 1.0);
-
-   GLfloat x = -250.0;
-   GLfloat y;
-   GLfloat yild = 20.0;
-   y = 275.0-font*120.0-j*yild;
-   if (t == 2) {
-     glEnable(GL_TEXTURE_2D);
-     glEnable(GL_BLEND);
-     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-   }
-   glPushMatrix(); {
-     glTranslatef(x, y, z);
-     fonts->Render(s);
-   } glPopMatrix();
-   if (font == 2) {
-     glDisable(GL_TEXTURE_2D);
-     glDisable(GL_BLEND);
-   }
-}
-
-*/
-

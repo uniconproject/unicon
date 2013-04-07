d613c4046d64ab0a3eb4872567cab1ccb1851275
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2013-04-07 18:44:08 +0000

    add Arch to -features

--- src/runtime/keyword.r
+++ src/runtime/keyword.r
@@ -422,6 +422,28 @@ keyword{1,*} features
 }
 #endif					/* REPO_REVISION */
 
+{
+         char *s, *ss, *sss;
+
+#if ARM 
+         sss = "Arch arm";
+#else
+      	 sss = "Arch x86";
+#endif
+
+#if WordBits == 16
+    	 ss = "_16";
+#elif WordBits == 32
+         ss = "_32";
+#elif WordBits == 64
+         ss = "_64";
+#endif
+	 s = alcstr(NULL, strlen(ss) + strlen(sss) + 1);
+	 strcpy(s, sss);
+	 strcat(s, ss);
+	 suspend C_string s;
+}
+
 #if defined(MSWindows) && defined(SM_DIGITIZER)
       {
       int value = GetSystemMetrics(SM_DIGITIZER);

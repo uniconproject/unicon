56c0861ae1ad68a65973b79bd527028ca3ab4741
Clinton Jeffery <clint.jeffery@gmail.com>
2013-05-16 06:07:07 +0000

    change default behavior to match Mingw32 standard/included headers

--- config/win32/gcc/define32.h
+++ config/win32/gcc/define32.h
@@ -1,10 +1,22 @@
+/*
+ * This header lists optional symbols for 32-bit builds.
+ *
+ * The rule of thumb we aim for is: it includes everything that comes with
+ * mingw out of the box. This includes pthreads, apparently, and 2D and 3D
+ * (OpenGL) headers, but not png, libz, or jpeg.
+ *
+ * In order to obtain png, libz, or jpeg libraries and headers for Mingw:
+ *
+ * ... fill in missing information here ...
+ */
+
 
 /*define for png image support, libz is required by PNG */
-#define HAVE_LIBPNG 1
-#define HAVE_LIBZ 1    /* DO NOT define if you are using zlib1.dll */
+/* #define HAVE_LIBPNG 1 */
+/* #define HAVE_LIBZ 1   */ /* DO NOT define if you are using zlib1.dll */
 
 /*define for jpeg image support */
-#define HAVE_LIBJPEG 1
+/* #define HAVE_LIBJPEG 1 */
 
 /*define if you have pthreads and want concurrency*/
 #define HAVE_LIBPTHREAD 1

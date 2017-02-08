e41b8de2ff9d328c038310838b568c378cf80004
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2017-02-08 06:13:18 +0000

    Simple plugin template

---
+++ plugins/src/template/template.c
@@ -0,0 +1,26 @@
+/***
+ *	Subject    : Unicon Plugin Template
+ *	Author     : Jafar Al-Gharaibeh
+ *	Date       : Feb 3, 2017
+ ***
+ *
+ *	Description: This is a template a Unicon plugin.
+ *
+ ***
+ *
+ *      This file is in the public domain.
+ *
+ ***
+ */
+
+/**********************************************************/
+/*                  Plugin Implementation                 */
+/**********************************************************/
+
+#include "template.h"
+
+int foo(UARGS)
+   {
+   int n=0;
+   RetInteger(n);			/* return result */
+   }
---
+++ plugins/src/template/template.h
@@ -0,0 +1,55 @@
+/***
+ *	Subject    : Unicon Plugin Template
+ *	Author     : Jafar Al-Gharaibeh
+ *	Date       : Feb 3, 2017
+ ***
+ *
+ *	Description: This is a template a Unicon plugin.
+ *		    
+ ***
+ *
+ *      This file is in the public domain.
+ *
+ ***
+ */
+
+#include "../icall.h"
+
+/*
+ * data types reference:
+ *   i : integer
+ *   r : real
+ *   n : number
+ *   s : string
+ *   l : list 
+ *   t : table 
+ *   x : any
+ * ...
+
+ * return signatures
+ *   function{0} return 0 result, i.e fail
+ *   function{1} return 1 result
+ *   function{0, 1} return 1 result or fail 
+
+ *   param signature
+ *   name@type              => param name and type, and it is required
+ *   name@type:default      => param name and type with a default value
+ *                             presence of default means the param is optional
+ */
+
+/**********************************************************/
+/*                      Plugin API                        */
+/**********************************************************/
+
+/*
+ *  @function{1} foo(value@i)
+ *  @return : i
+ *
+ *  One line describtion: does this and that
+ *
+ *  elaborate describon does this and that
+ *  and even more
+ *  but can't do these
+ *
+ */
+int foo(UARGS);

174f6eca9e9f84f2fb690c1c285688596a0d1031
Clinton Jeffery <clint.jeffery@gmail.com>
2017-05-23 20:15:25 +0000

    remove USE_LRINT ifdef

--- src/lib/audio/base.h
+++ src/lib/audio/base.h
@@ -18,19 +18,6 @@ extern "C" {
 #include "config.h"
 #endif /* DARWIN_PBBUILDER */
 
-
-#define USE_LRINT 0 /* icculus look here JIV FIXME */
-
-#if USE_LRINT
-
-#define __USE_ISOC99 1
-#define _ISOC99_SOURCE 1
-#define __USE_EXTERN_INLINES 1
-#define __FAST_MATH__ 1
-#include <math.h>
-
-#endif
-
 #ifdef DMALLOC
 /* do nothing */
 #undef malloc

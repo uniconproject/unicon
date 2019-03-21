ef38b4f286ea76596975217c8ed77f9cf506700e
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2019-03-21 04:04:08 +0000

    define some symbols just in case we use good old mingw32

--- src/h/sys.h
+++ src/h/sys.h
@@ -115,6 +115,47 @@
    #define Type(d) (int)((d).dword & TypeMask)
    #undef lst1
    #undef lst2
+
+   /*MinGW32 needs these defined*/
+   #ifndef EWOULDBLOCK
+      
+      #define EWOULDBLOCK             WSAEWOULDBLOCK
+      #define EINPROGRESS             WSAEINPROGRESS
+      #define EALREADY                WSAEALREADY
+      #define ENOTSOCK                WSAENOTSOCK
+      #define EDESTADDRREQ            WSAEDESTADDRREQ
+      #define EMSGSIZE                WSAEMSGSIZE
+      #define EPROTOTYPE              WSAEPROTOTYPE
+      #define ENOPROTOOPT             WSAENOPROTOOPT
+      #define EPROTONOSUPPORT         WSAEPROTONOSUPPORT
+      #define ESOCKTNOSUPPORT         WSAESOCKTNOSUPPORT
+      #define EOPNOTSUPP              WSAEOPNOTSUPP
+      #define EPFNOSUPPORT            WSAEPFNOSUPPORT
+      #define EAFNOSUPPORT            WSAEAFNOSUPPORT
+      #define EADDRINUSE              WSAEADDRINUSE
+      #define EADDRNOTAVAIL           WSAEADDRNOTAVAIL
+      #define ENETDOWN                WSAENETDOWN
+      #define ENETUNREACH             WSAENETUNREACH
+      #define ENETRESET               WSAENETRESET
+      #define ECONNABORTED            WSAECONNABORTED
+      #define ECONNRESET              WSAECONNRESET
+      #define ENOBUFS                 WSAENOBUFS
+      #define EISCONN                 WSAEISCONN
+      #define ENOTCONN                WSAENOTCONN
+      #define ESHUTDOWN               WSAESHUTDOWN
+      #define ETOOMANYREFS            WSAETOOMANYREFS
+      #define ETIMEDOUT               WSAETIMEDOUT
+      #define ECONNREFUSED            WSAECONNREFUSED
+      #define ELOOP                   WSAELOOP
+      #define EHOSTDOWN               WSAEHOSTDOWN
+      #define EHOSTUNREACH            WSAEHOSTUNREACH
+      #define EPROCLIM                WSAEPROCLIM
+      #define EUSERS                  WSAEUSERS
+      #define EDQUOT                  WSAEDQUOT
+      #define ESTALE                  WSAESTALE
+      #define EREMOTE                 WSAEREMOTE
+#endif						/* EWOULDBLOCK */
+   
 #endif					/* MSDOS */
 
 #if UNIX

a7078ee448db0c654ee287bd1d3d6e782c133f07
Don Ward <don@careful.co.uk>
2022-03-22 13:15:55 +0000

    Add rnglib <-> runtime API and new standard functions.
    Add runtime routines callable by a rnglib:
       getRngState
       getInitialBits
       putErrorCode
    Add rnglib routines callable by the runtime:
       startRng
       putSeed
       getRandomFpt
       getErrorText
       getRandomBits  -- optional
       getRandomInt   -- optional
    
    Add new standard functions:
       loadrng
       rngbits
       rngbitstring
       rngval

--- src/h/rproto.h
+++ src/h/rproto.h
@@ -92,6 +92,17 @@ void             set_gzerrortext(gzFile f);
 
 struct b_cons   *alccons        (union block *);
 
+#ifdef RngLibrary
+struct b_external *alcext(uword size);
+void * tryLoad(struct descrip *name, const char *format, char *path);
+void *getRngState( void);
+void putErrorCode(int n);
+int rngAsgnState(struct threadstate *ts, struct descrip v);
+/* These are for debugging and will eventually disapear */
+void pstr(struct descrip str);
+void dbgplibchain(struct b_cons *p, int level);
+#endif                                  /* RngLibrary */
+
 int             anycmp          (dptr dp1,dptr dp2);
 int             anycmpBase      (dptr dp1,dptr dp2,int sortType);
 #ifdef Arrays

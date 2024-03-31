375a0e7e2e00f68f7ee8c40713b2d0c04f3f9a4e
Don Ward <don@careful.co.uk>
2024-03-31 21:57:29 +0100

    XPM: Fix -Wmisleading-indentation in CrDatFrI.c

--- src/xpm/lib/CrDatFrI.c
+++ src/xpm/lib/CrDatFrI.c
@@ -194,10 +194,10 @@ XpmCreateDataFromXpmImage(char ***data_return, XpmImage *image, XpmInfo *info)
 /* exit point, free only locally allocated variables */
 exit:
     if (header) {
-        for (l = 0; l < header_nlines; l++)
-            if (header[l])
-                XpmFree(header[l]);
-                XpmFree(header);
+      for (l = 0; l < header_nlines; l++) {
+        if (header[l]) XpmFree(header[l]);
+      }
+      XpmFree(header);
     }
     return(ErrorStatus);
 }

e751cf0b14b4e6b10ad74398c92bf181162bdb70
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2026-04-30 20:16:17 -0500

    rtt: regenerate yyerror.c for Bison parser; document merr and YYUNDEF
    Regenerate yyerror.c with merr -B so errtab state keys match the current
    automaton; size yymaxstate/errtab to YYNSTATES - 1 / YYNSTATES from rttparse.c.
    Include stdlib.h for calloc.
    
    rttgram.y: note that token code 368 is reserved as YYUNDEF.
    
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- src/rtt/yyerror.c
+++ src/rtt/yyerror.c
@@ -1,131 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int yyerror_isinitialized, yymaxstate = 739;
+/*
+ * yymaxstate must be YYNSTATES - 1 from the committed rttparse.c so every
+ * parser state index is in range for _yyerror's errtab lookup.  Slots without
+ * merr-generated rows remain zero-initialized; see yyerror_init below.
+ */
+int yyerror_isinitialized, yymaxstate = 842;
 struct errtable {
    int i;
    union {
       char *msg;
       struct errtable *p;
       } u;
-   } errtab[740];
+   } errtab[843];
 void yyerror_init()
 {
-   errtab[94].i = 1;
-   errtab[94].u.msg = "syntax error";
-   errtab[136].i = 1;
-   errtab[136].u.msg = "bad enumeration";
-   errtab[246].i = 1;
-   errtab[246].u.msg = "syntax error";
-   errtab[52].i = 1;
-   errtab[52].u.msg = "syntax error";
-   errtab[99].i = 1;
-   errtab[99].u.msg = "bad enumeration type";
-   errtab[141].i = 1;
-   errtab[141].u.msg = "syntax error";
-   errtab[235].i = 1;
-   errtab[235].u.msg = "syntax error";
-   errtab[366].i = 1;
-   errtab[366].u.msg = "syntax error";
+   errtab[167].i = 1;
+   errtab[167].u.msg = "syntax error";
    errtab[544].i = 1;
    errtab[544].u.msg = "syntax error";
-   errtab[15].i = 1;
-   errtab[15].u.msg = "global declaration expected";
-   errtab[41].i = 1;
-   errtab[41].u.msg = "syntax error";
-   errtab[62].i = 1;
-   errtab[62].u.msg = "syntax error";
-   errtab[109].i = 1;
-   errtab[109].u.msg = "syntax error";
-   errtab[413].i = 1;
-   errtab[413].u.msg = "syntax error";
-   errtab[502].i = 1;
-   errtab[502].u.msg = "syntax error";
-   errtab[638].i = 1;
-   errtab[638].u.msg = "syntax error";
-   errtab[25].i = 1;
-   errtab[25].u.msg = "global declaration expected";
-   errtab[9].i = 1;
-   errtab[9].u.msg = "syntax error";
-   errtab[30].i = 1;
-   errtab[30].u.msg = "bad structure declaration";
-   errtab[140].i = 1;
-   errtab[140].u.msg = "syntax error";
-   errtab[234].i = 1;
-   errtab[234].u.msg = "syntax error";
-   errtab[365].i = 1;
-   errtab[365].u.msg = "bad function prototype";
-   errtab[103].i = 1;
-   errtab[103].u.msg = "syntax error";
-   errtab[328].i = 1;
-   errtab[328].u.msg = "syntax error";
-   errtab[370].i = 1;
-   errtab[370].u.msg = "syntax error";
+   errtab[125].i = 1;
+   errtab[125].u.msg = "syntax error";
+   errtab[261].i = 1;
+   errtab[261].u.msg = "syntax error";
+   errtab[481].i = 1;
+   errtab[481].u.msg = "syntax error";
+   errtab[88].i = 1;
+   errtab[88].u.msg = "bad declaration list";
+   errtab[93].i = 2;
+   errtab[93].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[93].u.p[0].u.msg = "syntax error";
+   errtab[93].u.p[1].i = 310;
+   errtab[93].u.p[1].u.msg = "syntax error";
+   errtab[93].u.p[2].i = 257;
+   errtab[93].u.p[2].u.msg = "syntax error";
+   errtab[182].i = 1;
+   errtab[182].u.msg = "syntax error";
+   errtab[108].i = 1;
+   errtab[108].u.msg = "global declaration expected";
+   errtab[192].i = 1;
+   errtab[192].u.msg = "bad enumeration";
    errtab[506].i = 1;
    errtab[506].u.msg = "syntax error";
-   errtab[134].i = 1;
-   errtab[134].u.msg = "syntax error";
-   errtab[155].i = 1;
-   errtab[155].u.msg = "illegal array declaration";
-   errtab[249].i = 1;
-   errtab[249].u.msg = "syntax error";
-   errtab[13].i = 1;
-   errtab[13].u.msg = "illegal structure declaration";
-   errtab[60].i = 1;
-   errtab[60].u.msg = "global declaration expected";
-   errtab[123].i = 1;
-   errtab[123].u.msg = "syntax error";
-   errtab[128].i = 1;
-   errtab[128].u.msg = "syntax error";
-   errtab[149].i = 1;
-   errtab[149].u.msg = "syntax error";
-   errtab[212].i = 1;
-   errtab[212].u.msg = "illegal array declaration";
-   errtab[222].i = 1;
-   errtab[222].u.msg = "syntax error";
-   errtab[54].i = 1;
-   errtab[54].u.msg = "syntax error";
-   errtab[227].i = 1;
-   errtab[227].u.msg = "syntax error";
-   errtab[253].i = 1;
-   errtab[253].u.msg = "syntax error";
-   errtab[452].i = 1;
-   errtab[452].u.msg = "syntax error";
-   errtab[59].i = 1;
-   errtab[59].u.msg = "syntax error";
-   errtab[101].i = 1;
-   errtab[101].u.msg = "syntax error";
-   errtab[216].i = 1;
-   errtab[216].u.msg = "syntax error";
-   errtab[504].i = 1;
-   errtab[504].u.msg = "syntax error";
-   errtab[373].i = 1;
-   errtab[373].u.msg = "syntax error";
-   errtab[640].i = 1;
-   errtab[640].u.msg = "syntax error";
-   errtab[687].i = 1;
-   errtab[687].u.msg = "syntax error";
-   errtab[48].i = 1;
-   errtab[48].u.msg = "syntax error";
-   errtab[95].i = 1;
-   errtab[95].u.msg = "syntax error";
-   errtab[739].i = 1;
-   errtab[739].u.msg = "syntax error";
-   errtab[53].i = 1;
-   errtab[53].u.msg = "bad declaration list";
-   errtab[58].i = 1;
-   errtab[58].u.msg = "syntax error";
-   errtab[147].i = 1;
-   errtab[147].u.msg = "syntax error";
-   errtab[42].i = 1;
-   errtab[42].u.msg = "syntax error";
-   errtab[110].i = 1;
-   errtab[110].u.msg = "syntax error";
-   errtab[215].i = 1;
-   errtab[215].u.msg = "syntax error";
-   errtab[639].i = 1;
-   errtab[639].u.msg = "syntax error";
-   errtab[686].i = 1;
-   errtab[686].u.msg = "syntax error";
+   errtab[165].i = 2;
+   errtab[165].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[165].u.p[0].u.msg = "syntax error";
+   errtab[165].u.p[1].i = 269;
+   errtab[165].u.p[1].u.msg = "syntax error";
+   errtab[165].u.p[2].i = 310;
+   errtab[165].u.p[2].u.msg = "syntax error";
+   errtab[254].i = 1;
+   errtab[254].u.msg = "syntax error";
+   errtab[479].i = 1;
+   errtab[479].u.msg = "syntax error";
+   errtab[704].i = 1;
+   errtab[704].u.msg = "syntax error";
+   errtab[175].i = 1;
+   errtab[175].u.msg = "syntax error";
+   errtab[75].i = 1;
+   errtab[75].u.msg = "bad enumeration type";
+   errtab[159].i = 1;
+   errtab[159].u.msg = "syntax error";
+   errtab[269].i = 2;
+   errtab[269].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[269].u.p[0].u.msg = "illegal array declaration";
+   errtab[269].u.p[1].i = 269;
+   errtab[269].u.p[1].u.msg = "illegal array declaration";
+   errtab[269].u.p[2].i = 257;
+   errtab[269].u.p[2].u.msg = "syntax error";
+   errtab[295].i = 1;
+   errtab[295].u.msg = "syntax error";
+   errtab[363].i = 1;
+   errtab[363].u.msg = "syntax error";
+   errtab[546].i = 1;
+   errtab[546].u.msg = "syntax error";
+   errtab[656].i = 1;
+   errtab[656].u.msg = "syntax error";
+   errtab[85].i = 1;
+   errtab[85].u.msg = "syntax error";
+   errtab[174].i = 1;
+   errtab[174].u.msg = "syntax error";
+   errtab[378].i = 2;
+   errtab[378].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[378].u.p[0].u.msg = "bad function prototype";
+   errtab[378].u.p[1].i = 258;
+   errtab[378].u.p[1].u.msg = "bad function prototype";
+   errtab[378].u.p[2].i = 257;
+   errtab[378].u.p[2].u.msg = "syntax error";
+   errtab[404].i = 2;
+   errtab[404].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[404].u.p[0].u.msg = "syntax error";
+   errtab[404].u.p[1].i = 269;
+   errtab[404].u.p[1].u.msg = "syntax error";
+   errtab[404].u.p[2].i = 257;
+   errtab[404].u.p[2].u.msg = "syntax error";
+   errtab[37].i = 1;
+   errtab[37].u.msg = "illegal structure declaration";
+   errtab[189].i = 1;
+   errtab[189].u.msg = "syntax error";
+   errtab[477].i = 1;
+   errtab[477].u.msg = "syntax error";
+   errtab[42].i = 2;
+   errtab[42].u.p = (struct errtable *)calloc(1,3 * sizeof(struct errtable));
+   errtab[42].u.p[0].u.msg = "global declaration expected";
+   errtab[42].u.p[1].i = 261;
+   errtab[42].u.p[1].u.msg = "global declaration expected";
+   errtab[42].u.p[2].i = 258;
+   errtab[42].u.p[2].u.msg = "global declaration expected";
 }
 
 int __merr_errors;
@@ -140,11 +122,7 @@ int _yyerror(char *s, int state)
    if (__merr_errors++ > 10) {
       fprintf(stderr, "too many errors, aborting");
       exit(__merr_errors); }
-   if (!yylval.t)
-     fprintf(stderr, "unexpected end of file:");
-   else
-     if(yylval.t->fname) fprintf(stderr, "%s:", yylval.t->fname);
-
+   if (yylval.t->fname) fprintf(stderr, "%s:", yylval.t->fname);
    if ((!strcmp(s, "syntax error") || !strcmp(s,"parse error"))&&
          (state>=0 && state<=yymaxstate)) {
        if (errtab[state].i==1)
@@ -161,9 +139,6 @@ int _yyerror(char *s, int state)
       sprintf(sbuf,"%s (%d;%d)", s, state, yychar);
       s=sbuf;
       }
-      if (yylval.t)
-        fprintf(stderr, "%d: # \"%s\": %s\n", yylval.t->line, yylval.t->image, s);
-      else
-        fprintf(stderr, "%s\n", s);
+   fprintf(stderr, "%d: # \"%s\": %s\n", yylval.t->line, yylval.t->image, s);
    return 0;
 }
